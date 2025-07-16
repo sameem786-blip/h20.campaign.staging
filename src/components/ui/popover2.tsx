7b5e9959026b77f51bbbc05a6ee98a24e2fb2934
Sameem Abbas <sameemabbas2002@outlook.com>
2025-07-17 02:21:07 +0500

    fix

--- src/components/ui/popover2.tsx
+++ src/components/ui/popover2.tsx
@@ -19,11 +19,8 @@ const Popover = ({ className, offset = 4, ...props }: AriaPopoverProps) => (
     className={composeRenderProps(className, (className) =>
       cn(
         "z-50 rounded-md border bg-popover text-popover-foreground shadow-md outline-none",
-        /* Entering */
         "data-[entering]:animate-in data-[entering]:fade-in-0 data-[entering]:zoom-in-95",
-        /* Exiting */
         "data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95",
-        /* Placement */
         "data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
         className,
       ),
@@ -32,9 +29,15 @@ const Popover = ({ className, offset = 4, ...props }: AriaPopoverProps) => (
   />
 )
 
-function PopoverDialog({ className, ...props }: AriaDialogProps) {
-  return <AriaDialog className={cn("p-4 outline outline-0", className)} {...props} />
-}
+// ✅ Fixed: now forwards the ref properly
+const PopoverDialog = React.forwardRef<
+  React.ElementRef<typeof AriaDialog>,
+  React.ComponentPropsWithoutRef<typeof AriaDialog>
+>(({ className, ...props }, ref) => (
+  <AriaDialog ref={ref} className={cn("p-4 outline outline-0", className)} {...props} />
+))
+
+PopoverDialog.displayName = "PopoverDialog"
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof AriaDialog>,
